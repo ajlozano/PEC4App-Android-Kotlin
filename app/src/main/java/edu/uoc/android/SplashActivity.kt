2c7949e872528c5b77b94a9243f08757cb4b5d4f
Toni <ajlozano@users.noreply.github.com>
2022-09-16 13:22:41 +0200

    Fixed splash screen implementation and designed Main Menu layout.

---
+++ app/src/main/java/edu/uoc/android/SplashActivity.kt
@@ -0,0 +1,20 @@
+package edu.uoc.android
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.postDelayed
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler(Looper.myLooper()!!).postDelayed({ // This method will be executed once the timer is over
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}
