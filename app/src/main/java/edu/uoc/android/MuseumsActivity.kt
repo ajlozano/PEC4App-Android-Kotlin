9d25d2595e93b061bda06620cf7bb3727bc38442
Toni <ajlozano@users.noreply.github.com>
2022-09-20 17:39:29 +0200

    Added progress bar on MuseumActivity

--- app/src/main/java/edu/uoc/android/MuseumsActivity.kt
+++ app/src/main/java/edu/uoc/android/MuseumsActivity.kt
@@ -7,6 +7,8 @@ import android.os.Bundle
 import android.renderscript.ScriptGroup
 import android.telecom.Call
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import androidx.constraintlayout.helper.widget.Carousel
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.tabs.TabLayout
@@ -18,35 +20,40 @@ import retrofit2.Response
 import javax.security.auth.callback.Callback
 
 class MuseumsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMuseumsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         callbackManager(binding)
     }
 
     private fun callbackManager(binding: ActivityMuseumsBinding) {
         binding.museumRecycler.layoutManager = LinearLayoutManager(this)
-
         val call = museumAPI.museums("1","25")
-
         call?.enqueue(object : retrofit2.Callback<Museums?> {
             override fun onResponse(call: retrofit2.Call<Museums?>, response: Response<Museums?>) {
                 if (response.code() == 200) {
-                    //show Progress(false)
+                    showProgress(false, binding)
                     val museums = response.body()!!
                     val adapter = museumsAdapter()
                     binding.museumRecycler.adapter = adapter
                     adapter.submitList(museums.elements)
-//                    Log.d(TAG, "Number of images in museums.elements: " +
-//                            "${museums.elements[0].imatge.size}")
                 }
             }
             override fun onFailure(call: retrofit2.Call<Museums?>, t: Throwable) {
                  Log.d(TAG, "Retrofit2.Call: Failled to enqueue")
             }
         })
+    }
+
+    private fun showProgress(state: Boolean, binding: ActivityMuseumsBinding) {
+        val progressBar : ProgressBar
+        if(state) {
+            binding.museumProgress.visibility = View.VISIBLE
+        } else {
+            binding.museumProgress.visibility = View.GONE
+        }
     }
 }
 
