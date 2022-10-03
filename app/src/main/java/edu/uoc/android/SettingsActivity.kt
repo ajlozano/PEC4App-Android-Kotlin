464b2f9430871bf73efa6f9a3cac2f4b0a797e2e
Toni <ajlozano@users.noreply.github.com>
2022-10-03 19:41:01 +0200

    Stored image from camera to external storage.

--- app/src/main/java/edu/uoc/android/SettingsActivity.kt
+++ app/src/main/java/edu/uoc/android/SettingsActivity.kt
@@ -1,34 +1,35 @@
 package edu.uoc.android
 
-import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
+import android.os.Environment
 import android.view.View.GONE
-import android.view.View.VISIBLE
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.result.ActivityResult
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.drawToBitmap
+import androidx.core.content.FileProvider
 import edu.uoc.android.databinding.ActivitySettingsBinding
+import java.io.File
 
-private const val REQUEST_IMAGE_CAPTURE = 2000
+const val FILENAME = "imageapp"
 
 class SettingsActivity : AppCompatActivity() {
-//    val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//    val getContent = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) {
-//        bitmap ->
-//
-//    }
     private lateinit var imageView: ImageView
     private lateinit var textImage: TextView
     private var imageBitmap: Bitmap? = null
-    val getContent = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) {
-        bitmap ->  imageBitmap = bitmap
-        textImage.visibility = GONE
-        imageView.setImageBitmap(imageBitmap)
+    private var picturePath = ""
+
+    val getContent = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+        success ->
+        if (success) {
+            checkPicturePath()
+            Toast.makeText(this, "Image saved!", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,16 +40,60 @@ class SettingsActivity : AppCompatActivity() {
 
         imageView = binding.image
         textImage = binding.imageText
-
-        if (imageView.drawable != null) {
-
-        }
+//        getBitmapFromStorage()
+//        imageView.setImageBitmap(imageBitmap)
         binding.button.setOnClickListener {
             openCamera()
         }
     }
 
+//    private fun getBitmapFromStorage() {
+//        val photoPath = Environment.getExternalStorageDirectory().toString() + "/" + FILENAME + ".jpg"
+//        if (photoPath.isNotEmpty()) {
+//            imageBitmap = BitmapFactory.decodeFile(photoPath)
+//            Toast.makeText(this, "HAS FILE", Toast.LENGTH_LONG).show()
+//
+//        }
+//    }
+
     private fun openCamera() {
-        getContent.launch(null)
+        val file = createImageFile(checkFile())
+        val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            FileProvider.getUriForFile(this, "$packageName.provider", file)
+        } else  {
+            Uri.fromFile(file)
+        }
+        getContent.launch(uri)
+    }
+
+    private fun checkFile(): File {
+        val fileDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES + "/UOCImageApp")
+        if (fileDirectory != null) {
+            if (fileDirectory.listFiles() != null) {
+                Toast.makeText(
+                    this, "Filename: ${fileDirectory.absolutePath}",
+                    Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "HAS FILE", Toast.LENGTH_LONG).show()
+            }
+        }
+        return fileDirectory!!
+    }
+
+    private fun checkPicturePath() {
+        if (picturePath.isNotEmpty()) {
+            imageBitmap = BitmapFactory.decodeFile(picturePath)
+            textImage.visibility = GONE
+            imageView.setImageBitmap(imageBitmap)
+        } else {
+            Toast.makeText(this, "No Image selected", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun createImageFile(fileDirectory: File?): File {
+        val fileName = FILENAME
+        val file = File.createTempFile(fileName, ".jpg", fileDirectory)
+        picturePath = file.absolutePath
+        return file
     }
 }
