9ff28d6e6e7d01bfe4a57fd8fe0b7cfd8d89670b
Toni <aj.lozano.fernandez@gmail.com>
2022-10-31 20:15:02 +0100

    Added MockServer Unit Test and Google analytics custom events.

--- app/src/main/java/edu/uoc/android/MainActivity.kt
+++ app/src/main/java/edu/uoc/android/MainActivity.kt
@@ -3,33 +3,69 @@ package edu.uoc.android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import edu.uoc.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var analytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Obtain the FirebaseAnalytics instance.
+        analytics = Firebase.analytics
+
+        val bundle: Bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Event.SCREEN_VIEW, "Google Maps custom event")
+        SendCustomEvent(bundle)
+
         binding.rellayMaps.setOnClickListener {
+            val bundle: Bundle = Bundle()
+            bundle.putString("GoggleMaps", "Google Maps custom event")
+            SendCustomEvent(bundle)
+
             val intent = Intent(this, GoogleMapsActivity::class.java)
             startActivity(intent)
         }
         binding.rellayMuseums.setOnClickListener {
+            val bundle: Bundle = Bundle()
+            bundle.putString("Museus", "Museus custom event")
+            SendCustomEvent(bundle)
+
             val intent = Intent(this, MuseumsActivity::class.java)
             startActivity(intent)
         }
         binding.rellayQuizzes.setOnClickListener {
+            val bundle: Bundle = Bundle()
+            bundle.putString("Quizz", "Quizz custom event")
+            SendCustomEvent(bundle)
+
             val intent = Intent(this, QuizzActivity::class.java)
             startActivity(intent)
         }
         binding.rellaySettings.setOnClickListener {
+            val bundle: Bundle = Bundle()
+            bundle.putString("Settings", "Settings custom event")
+            SendCustomEvent(bundle)
+
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
         }
         binding.rellayFakebot.setOnClickListener {
+            val bundle: Bundle = Bundle()
+            bundle.putString("FakeBot", "Fake Bot custom event")
+            SendCustomEvent(bundle)
+
             val intent = Intent(this, FakeBotActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun SendCustomEvent(bundle: Bundle) {
+        analytics.logEvent("Custom_Event", bundle)
     }
 }
--- app/src/test/java/edu/uoc/android/MockServerUnitTest.kt
+++ app/src/test/java/edu/uoc/android/MockServerUnitTest.kt
@@ -1,5 +1,52 @@
 package edu.uoc.android
 
+import android.content.ContentProviderOperation.newCall
+import com.google.android.gms.common.api.Api
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+
 class MockServerUnitTest {
+    // Create a MockWebServer. These are lean enough that you can create a new
+    // instance for every unit test.
+    val server = MockWebServer()
+
+    val dispatcher: Dispatcher = object : Dispatcher() {
+        @Throws(InterruptedException::class)
+        override fun dispatch(request: RecordedRequest): MockResponse {
+            when (request.path) {
+                "/api/dataset/museus/" -> return MockResponse().setResponseCode(200)
+                    .setBody(Utils.BODY)
+            }
+            return MockResponse().setResponseCode(404)
+        }
+    }
+
+    @Before
+    fun SetupMockServer(){
+        server.start()
+        server.dispatcher = dispatcher
+    }
+
+    @Test
+    fun MuseumMockServerTesting () {
+        val url = server.url( "/api/dataset/museus/")
+
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(url)
+            .build()
+        val call = client.newCall(request)
+        val response = call.execute()
+
+        Assert.assertEquals(Utils.BODY, response.body?.string())
+    }
 
 }
